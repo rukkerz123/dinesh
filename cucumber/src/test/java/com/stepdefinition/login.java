0f241493d3217997cbcb6be216d852b4b2d7d2b0
rukkerz123 <118416490+rukkerz123@users.noreply.github.com>
2023-03-02 17:31:06 +0530

    Update login.java
    updated software

--- cucumber/src/test/java/com/stepdefinition/login.java
+++ cucumber/src/test/java/com/stepdefinition/login.java
@@ -2,6 +2,7 @@ package com.stepdefinition;
 
 import baseclass.baseclass;
 
-public class login extends  baseclass  {
+public class login extends  baseclass  { 
+  system.out.println("my name is srinath");
 
 }
